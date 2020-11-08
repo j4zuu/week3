96a76454223a6e85d42e428d0584b067edfa33cb
j4zuu <jasu.honkanen2@gmail.com>
2020-11-08 16:12:20 +0200

    should work

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('uploads'))
 
 app.use('/', rootRoute);
+/*
 app.use('/cat', passport.authenticate('jwt' , {session: false}), catRoute);
 app.use('/user', passport.authenticate('jwt' , {session: false}), userRoute)
-
+*/
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
